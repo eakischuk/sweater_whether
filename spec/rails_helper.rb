973fe6d33efe73f3ff9f6ee5460251ee2f2478d5
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-13 20:42:39 -0700

    Fix private config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,7 +76,8 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
-#  config.filter_sensitive_data('<api_key>') { ENV['pusher_key'] }
+  config.filter_sensitive_data('<api_key>') { ENV['MAP_QUEST_KEY'] }
+  config.filter_sensitive_data('<api_key>') { ENV['OPEN_WEATHER_KEY'] }
 #  config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end

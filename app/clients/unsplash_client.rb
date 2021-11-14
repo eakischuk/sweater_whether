a49e2e19702c1098023d024e337068a89a0dd025
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-13 22:13:39 -0700

    Establish connection with unsplash

---
+++ app/clients/unsplash_client.rb
@@ -0,0 +1,24 @@
+class UnsplashClient
+  def self.background(location)
+    params = {
+      query: location,
+      per_page: 1,
+      orientation: 'landscape'
+    }
+    response = conn.get('/search/photos', params)
+    parse_json(response)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(
+      url: 'https://api.unsplash.com',
+      params: { client_id: ENV['UNSPLASH_KEY'] }
+    )
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end

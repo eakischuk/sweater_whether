a822f196f2a95b471bd11b516cd125f3944fd508
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-13 21:03:53 -0700

    Add bad request error handling

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    forecast = ForecastFacade.weather_by_location(params[:location])
-    render json: ForecastSerializer.new(forecast)
+    if params[:location]
+      forecast = ForecastFacade.weather_by_location(params[:location])
+      render json: ForecastSerializer.new(forecast)
+    else
+      bad_request
+    end
   end
 end

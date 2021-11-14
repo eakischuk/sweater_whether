5f2203c9438613f71ebe2ae31a57f43985ff8b0e
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-13 22:50:37 -0700

    Reconfigure VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,9 +76,12 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
+  config.default_cassette_options = {
+    :match_requests_on => [:method, :uri],
+    :record => :new_episodes
+  }
   config.filter_sensitive_data('<api_key>') { ENV['MAP_QUEST_KEY'] }
   config.filter_sensitive_data('<api_key>') { ENV['OPEN_WEATHER_KEY'] }
   config.filter_sensitive_data('<api_key>') { ENV['UNSPLASH_KEY'] }
-#  config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end

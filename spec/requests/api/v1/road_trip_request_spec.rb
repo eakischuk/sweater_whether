0aa04aa64ff41a76aec6b838f8112dea07332972
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-14 22:08:04 -0700

    Add edge case impossible travel test

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -51,6 +51,34 @@ RSpec.describe 'RoadTrip API' do
     end
   end
   describe 'sad paths' do
+    it 'impossible trip' do
+      impossible_trip_params = {
+        origin: "Denver, CO",
+        destination: "London, UK",
+        api_key: @user.api_key
+      }
+      post '/api/v1/road_trip', headers: @headers, params: impossible_trip_params.to_json
+      expect(response).to be_successful
+      trip = JSON.parse(response.body, symbolize_names: true)
+      expect(trip).to be_a(Hash)
+      expect(trip).to have_key(:data)
+      expect(trip[:data]).to have_key(:id)
+      expect(trip[:data][:id]).to be(nil)
+      expect(trip[:data]).to have_key(:type)
+      expect(trip[:data][:type]).to be_a(String)
+      expect(trip[:data]).to have_key(:attributes)
+      expect(trip[:data][:attributes]).to be_a(Hash)
+      expect(trip[:data][:attributes]).to have_key(:start_city)
+      expect(trip[:data][:attributes][:start_city]).to be_a(String)
+      expect(trip[:data][:attributes]).to have_key(:end_city)
+      expect(trip[:data][:attributes][:end_city]).to be_a(String)
+      expect(trip[:data][:attributes]).to have_key(:travel_time)
+      expect(trip[:data][:attributes][:travel_time]).to eq('impossible')
+      expect(trip[:data][:attributes]).to have_key(:weather_at_eta)
+      expect(trip[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+      expect(trip[:data][:attributes][:weather_at_eta]).to_not have_key(:temperature)
+      expect(trip[:data][:attributes][:weather_at_eta]).to_not have_key(:conditions)
+    end
     it '401 without authorization' do
       unauthorized_params = {
         origin: "Denver, CO",
